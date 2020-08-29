4f50252c3fc456df4dd5cb41216747bd67758b99
psdhanesh7 <psdhanesh2000@gmail.com>
2020-08-29 19:25:30 +0530

    Re commit

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://psdhanesh7:h19yeaSNu5PyWPMf@cluster0.llxb8.mongodb.net/medicab-dev?retryWrites=true&w=majority",
-    sessionSecret: 'this is really an awesome secret'
-}
