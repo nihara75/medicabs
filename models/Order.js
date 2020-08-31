1bbdac6dbbf689f583ba1d72cd7728c0b80d8cf6
nihara75 <niharar25@gmail.com>
2020-08-31 18:26:31 +0530

    added order and partner schema

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+
+const orderSchema= new Schema({
+orderid:Integer,
+lalitude:Integer,
+longitude:Integer,
+place:String,
+storeid:{
+          type:Integer,
+          required:true
+                },
+image:String ,        // prescription storing
+cancel:Boolean       //to place and cancel
+
+
+});
+
+mongoose.model("Order",orderSchema);
 
 // place order
 // cancel
--- models/Partner.js
+++ models/Partner.js
@@ -1,4 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const Order = require('mongoose').model('Order');
+const Medicine = require('mongoose').model('Medicine');
 
+
+const partnerSchema = new Schema({
+  partnerid:Integer,
+  order:Order,  //Order object
+  medicine:Medicine,
+  status:{
+          type:Boolean,
+          required:true
+        },
+  closed:Boolean
+
+});
+
+
+mongoose.model("Partner",partnerSchema);
 
 
 
