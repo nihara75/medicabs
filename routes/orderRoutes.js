43e7e5f01c20eb98499ba48fc75fbd622bf22cc1
nihara75 <niharar25@gmail.com>
2020-09-11 16:26:16 +0530

    added route contents

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -29,28 +29,72 @@ app.post('/photo',function(req,res){
 
 
 // Route to get all the active requirements for a particular shop
-router.get('/active', (req, res) => {
+router.get('/active', async (req, res) => {
+  const shopId=req.query.shopId;
+  try{
+    await Order.find({partner:shopId,active:true},(err,details)=>{
+      console.log(details);
+    });
+  }catch(err){
+    console.log(err);
+  }
+
 	// shop id should be provided as a query param
 });
 
 // Route to get the details of all the closed requirements for a particular shop
-router.get('/closed', (req, res) => {
+router.get('/closed',async (req, res) => {
 	// shop id should be provided as a query param
+  const shopId=req.query.shopId;
+  try{
+
+await Order.find({partner:shopId,active:false},(err,closed)=>{
+  console.log(closed);
+});
+
+
+  }catch(err){
+    console.log(err);
+  }
+
 
 });
 
 // Route to place a new order
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const {user,partner,deliveryAddress}=req.body;
+  try{
+    const order=new Order({user,partner,deliveryAddress});
+    await order.save();
+  }catch(err){
+    console.log(err);
+  }
+
+
 
 });
 
 // Route to cancel a particular order by user
-router.put('/cancel/:orderId', (req, res) => {
-
+router.put('/cancel/:orderId', async (req, res) => {
+//const cancel=req.body.cancel;
+//const orderId=req.params.orderId;
+try{
+  await Order.updateOne({_id:req.params.orderId},{$set:{cancel:req.body.cancel}});
+}catch(err){
+  console.log(err);
+}
 });
 
 // Route to close a particular order
-router.put('/close/:orderId', (req, res) => {
+router.put('/close/:orderId',async (req, res) => {
+  try{
+
+          await Order.updateOne({_id:req.params.orderId},{$set:{actve:req.body.closed}});
+
+
+  }catch(err){
+    console.log(err);
+  }
 
 });
 
